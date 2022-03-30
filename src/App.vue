e6f1f9dbfceade7a2633e6c85f4d4ab38f83e350
BaikSeungJeon <tmdwjsofficial@naver.com>
2022-03-30 23:46:25 +0900

    0331

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
+  <div class="menu">
+    <a v-for="작명 in 3" :key="작명">Home</a>
+  </div>
   <img alt="Vue logo" src="./assets/logo.png">
   <div :style="color1">
     <h1>홍대 원룸</h1>
-    <p>{{ price1 }}</p>
+    <p>1억</p>
   </div>
   <div :style="color2">
     <h1>마포 원룸</h1>
-    <p>{{ price2 }}</p>
+    <p>2억</p>
+  </div>
+  <div :style="color1">
+    <h1>{{products[0]}} 원룸</h1>
+    <p>1억</p>
+  </div>
+  <div :style="color1">
+    <h1>{{products[1]}} 원룸</h1>
+    <p>1억</p>
+  </div>
+  <div :style="color2">
+    <h1>{{products[2]}} 원룸</h1>
+    <p>2억</p>
   </div>
 </template>
 
@@ -17,13 +32,13 @@ export default {
   data(){
     return {
       // 자료 이름: 자료 내용
-      price1: '2억',
-      price2: '1억',
       color1: 'color: green',
-      color2: 'color: purple'
+      color2: 'color: purple',
+      products:['강남', '판교', '마곡']
     }
   },
   components: {
+    
   }
 }
 </script>
@@ -37,4 +52,17 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.menu {
+  background-color: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: #fff;
+  padding: 10px;
+}
 </style>
+
+
+
+
